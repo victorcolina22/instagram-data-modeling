4116cc0bf612c575a0afc58d935ecd59920b7fcf
victorcolina22 <victorcolina2202@gmail.com>
2021-02-02 13:42:03 +0000

    Ejercicio data-modeling

--- src/models.py
+++ src/models.py
@@ -8,26 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(255))
+    password = Column(String(255))
+    firstname = Column(String(255))
+    lastname = Column(String(255))
+    email = Column(String(255))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    
+class Follower(Base):
+    __tablename__ = 'followers'
+    users_from_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    users_to_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    users_id = Column(Integer, ForeignKey('users.id'))
+
+
+class Media(Base):
+    __tablename__ = 'medias'
+    id = Column(Integer, primary_key=True)
+    media_type = Column(Integer)
+    media_url = Column(String(255))
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(300), nullable=True)
+    author_id = Column(Integer, ForeignKey('users.id'))
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
 render_er(Base, 'diagram.png')
